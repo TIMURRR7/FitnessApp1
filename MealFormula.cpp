9e16c0e1f7e726f41a05efd4a6d982e315f3f046
Timur <kalinovskiytimur@bk.ru>
2025-11-03 23:31:04 +0700

    Добавлен класс MealFormula: рецепты блюд, предпочтения пользователя

---
+++ MealFormula.cpp
@@ -0,0 +1,20 @@
+﻿#include "MealFormula.hpp"
+
+// Конструктор: Инициализирует поля
+MealFormula::MealFormula(const std::string& id, const std::string& dishName, const std::list<std::string>& componentList,
+    const std::string& nutritionPerPortion, int prepTimeMin)
+    : id(id), dishName(dishName), componentList(componentList), nutritionPerPortion(nutritionPerPortion),
+    prepTimeMin(prepTimeMin) {}
+
+// Деструктор: Пустой
+MealFormula::~MealFormula() {}
+
+// Метод: Пересчёт порций (заглушка)
+std::string MealFormula::adjustPortions(double portionFactor) const {
+    return "Adjusted"; // Пример
+}
+
+// Метод: Проверка предпочтений (заглушка)
+bool MealFormula::fitsUserPrefs(const ProfileManager& user) const {
+    return true;
+}
---
+++ MealFormula.hpp
@@ -0,0 +1,32 @@
+﻿#ifndef MEALFORMULA_HPP
+#define MEALFORMULA_HPP
+
+#include <string>
+#include <list>
+#include "ProfileManager.hpp" 
+
+// Класс MealFormula: Рецепт блюда
+class MealFormula {
+public:
+    // Конструктор: Инициализирует рецепт
+    MealFormula(const std::string& id, const std::string& dishName, const std::list<std::string>& componentList,
+        const std::string& nutritionPerPortion, int prepTimeMin);
+
+    // Деструктор
+    ~MealFormula();
+
+    // Метод: Пересчитывает нутриенты на порции 
+    std::string adjustPortions(double portionFactor) const;
+
+    // Метод: Проверяет соответствие предпочтениям пользователя
+    bool fitsUserPrefs(const ProfileManager& user) const;
+
+private:
+    std::string id; // Идентификатор
+    std::string dishName; // Название блюда
+    std::list<std::string> componentList; // Ингредиенты
+    std::string nutritionPerPortion; // Нутриенты на порцию
+    int prepTimeMin; // Время приготовления в минутах
+};
+
+#endif
