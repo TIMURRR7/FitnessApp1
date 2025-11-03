1a3d673dbb34ebeb7a7ac424ec2442a79d28f384
Timur <kalinovskiytimur@bk.ru>
2025-11-03 15:05:18 +0700

    Добавлен класс FitnessDatabase: поиск, фильтрация, добавление упражнений

---
+++ FitnessDatabase.cpp
@@ -0,0 +1,30 @@
+﻿#include "FitnessDatabase.hpp"
+
+// Конструктор: Устанавливает дату обновления по умолчанию
+FitnessDatabase::FitnessDatabase() : updateTimestamp("2025-10-21") {}
+
+// Деструктор
+FitnessDatabase::~FitnessDatabase() {}
+
+// Метод: Поиск по названию (заглушка)
+std::list<Activity> FitnessDatabase::findByName(const std::string& searchQuery) const {
+    std::list<Activity> result;
+    return result;
+}
+
+// Метод: Фильтрация (заглушка)
+std::list<Activity> FitnessDatabase::filterActivities(ProfileManager::Goal targetGoal, const std::set<ProfileManager::Equipment>& equip, ProfileManager::Level lvl) const {
+    std::list<Activity> result;
+    // Фильтрация
+    return result;
+}
+
+// Метод: Добавляет упражнение
+void FitnessDatabase::addActivity(const Activity& newActivity) {
+    activities.push_back(newActivity);
+}
+
+// Метод: Обновляет библиотеку
+void FitnessDatabase::updateLibrary(const std::list<Activity>& newActivities) {
+    activities = newActivities;
+}
---
+++ FitnessDatabase.hpp
@@ -0,0 +1,35 @@
+﻿#ifndef FITNESSDATABASE_HPP
+#define FITNESSDATABASE_HPP
+
+#include <list>
+#include <string>
+#include "Activity.hpp"
+#include "ProfileManager.hpp"
+
+// Класс FitnessDatabase: Репозиторий упражнений
+class FitnessDatabase {
+public:
+    // Конструктор: Инициализирует базу 
+    FitnessDatabase();
+
+    // Деструктор: Освобождает ресурсы
+    ~FitnessDatabase();
+
+    // Метод: Поиск упражнений по названию
+    std::list<Activity> findByName(const std::string& searchQuery) const;
+
+    // Метод: Фильтрация упражнений по цели, оборудованию и уровню
+    std::list<Activity> filterActivities(ProfileManager::Goal targetGoal, const std::set<ProfileManager::Equipment>& equip, ProfileManager::Level lvl) const;
+
+    // Метод: Добавляет новое упражнение
+    void addActivity(const Activity& newActivity);
+
+    // Метод: Обновляет всю библиотеку упражнений
+    void updateLibrary(const std::list<Activity>& newActivities);
+
+private:
+    std::list<Activity> activities; // Список упражнений
+    std::string updateTimestamp; // Дата последнего обновления
+};
+
+#endif
