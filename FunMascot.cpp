b5517bcb0e9afdd105eebaf1aeaa07f90b4836f6
TIMURRR7 <kalinovskiytimur@bk.ru>
2025-11-30 22:50:10 +0700

    Доработан класс FunMascot

--- FunMascot.cpp
+++ FunMascot.cpp
@@ -1,24 +1,20 @@
-﻿#include "FunMascot.hpp"
+#include "FunMascot.hpp"
 
 // Конструктор: Инициализирует поля
 FunMascot::FunMascot(const std::string& userId, MascotMood currentMood, int experiencePoints,
     const std::set<std::string>& unlockedSkins)
     : userId(userId), currentMood(currentMood), experiencePoints(experiencePoints), unlockedSkins(unlockedSkins) {}
 
+// Конструктор копирования
+FunMascot::FunMascot(const FunMascot& other)
+    : userId(other.userId), currentMood(other.currentMood), experiencePoints(other.experiencePoints),
+    unlockedSkins(other.unlockedSkins) {}
+
 // Деструктор
 FunMascot::~FunMascot() {}
 
-// Метод: Реакция на сессию (заглушка)
+
+// Метод: Реакция на сессию
 std::string FunMascot::respondToSession(TrainingSession::WorkoutStatus outcome) const {
-    return "Good job!"; // Пример
-}
-
-// Метод: Добавление опыта
-void FunMascot::addExperience(int points) {
-    experiencePoints += points;
-}
-
-// Метод: Генерирует мотивацию (заглушка)
-std::string FunMascot::generateMotivation(const ProfileManager& user, const ProgressTracker& tracker) const {
-    return "Keep going!"; // Пример
+    return "Good job!"; 
 }
