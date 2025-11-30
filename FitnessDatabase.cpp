6937cfa3cd0fabcf5e03c958740427cd03f81213
TIMURRR7 <kalinovskiytimur@bk.ru>
2025-11-30 22:47:39 +0700

    Доработан класс FitnessDatabase

--- FitnessDatabase.cpp
+++ FitnessDatabase.cpp
@@ -1,30 +1,40 @@
-﻿#include "FitnessDatabase.hpp"
+#include "FitnessDatabase.hpp"
+#include <algorithm>
+#include <stdexcept>
 
 // Конструктор: Устанавливает дату обновления по умолчанию
 FitnessDatabase::FitnessDatabase() : updateTimestamp("2025-10-21") {}
+
+// Конструктор копирования
+FitnessDatabase::FitnessDatabase(const FitnessDatabase& other)
+    : activities(other.activities), updateTimestamp(other.updateTimestamp) {}
 
 // Деструктор
 FitnessDatabase::~FitnessDatabase() {}
 
 // Метод: Поиск по названию (заглушка)
-std::list<Activity> FitnessDatabase::findByName(const std::string& searchQuery) const {
-    std::list<Activity> result;
-    return result;
-}
-
-// Метод: Фильтрация (заглушка)
-std::list<Activity> FitnessDatabase::filterActivities(ProfileManager::Goal targetGoal, const std::set<ProfileManager::Equipment>& equip, ProfileManager::Level lvl) const {
-    std::list<Activity> result;
-    // Фильтрация
-    return result;
+std::list<std::shared_ptr<Activity>> FitnessDatabase::findByName(const std::string& searchQuery) const {
+    try {
+        if (searchQuery.empty()) {
+            throw std::invalid_argument("Поисковый запрос не может быть пустым");
+        }
+        std::list<std::shared_ptr<Activity>> result;
+        return result;
+    }
+    catch (const std::exception& e) {
+        throw;
+    }
 }
 
 // Метод: Добавляет упражнение
-void FitnessDatabase::addActivity(const Activity& newActivity) {
-    activities.push_back(newActivity);
-}
-
-// Метод: Обновляет библиотеку
-void FitnessDatabase::updateLibrary(const std::list<Activity>& newActivities) {
-    activities = newActivities;
+void FitnessDatabase::addActivity(std::unique_ptr<Activity> newActivity) {
+    try {
+        if (!newActivity) {
+            throw std::invalid_argument("Упражнение не может быть пустым");
+        }
+        activities.push_back(std::shared_ptr<Activity>(std::move(newActivity)));
+    }
+    catch (const std::exception& e) {
+        throw;
+    }
 }
