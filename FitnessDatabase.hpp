364f0fde58e46da57703f959dfa90d5343bc4bad
TIMURRR7 <kalinovskiytimur@bk.ru>
2025-11-30 22:46:22 +0700

    Доработан класс FitnessDatabase

--- FitnessDatabase.hpp
+++ FitnessDatabase.hpp
@@ -1,34 +1,32 @@
-﻿#ifndef FITNESSDATABASE_HPP
+#ifndef FITNESSDATABASE_HPP
 #define FITNESSDATABASE_HPP
 
 #include <list>
 #include <string>
+#include <memory>
 #include "Activity.hpp"
 #include "ProfileManager.hpp"
 
 // Класс FitnessDatabase: Репозиторий упражнений
 class FitnessDatabase {
 public:
-    // Конструктор: Инициализирует базу 
+    // Конструктор: Инициализирует базу
     FitnessDatabase();
+
+    // Конструктор копирования
+    FitnessDatabase(const FitnessDatabase& other);
 
     // Деструктор: Освобождает ресурсы
     ~FitnessDatabase();
 
     // Метод: Поиск упражнений по названию
-    std::list<Activity> findByName(const std::string& searchQuery) const;
-
-    // Метод: Фильтрация упражнений по цели, оборудованию и уровню
-    std::list<Activity> filterActivities(ProfileManager::Goal targetGoal, const std::set<ProfileManager::Equipment>& equip, ProfileManager::Level lvl) const;
+    std::list<std::shared_ptr<Activity>> findByName(const std::string& searchQuery) const;
 
     // Метод: Добавляет новое упражнение
-    void addActivity(const Activity& newActivity);
-
-    // Метод: Обновляет всю библиотеку упражнений
-    void updateLibrary(const std::list<Activity>& newActivities);
+    void addActivity(std::unique_ptr<Activity> newActivity);
 
 private:
-    std::list<Activity> activities; // Список упражнений
+    std::list<std::shared_ptr<Activity>> activities; // Список упражнений
     std::string updateTimestamp; // Дата последнего обновления
 };
 
