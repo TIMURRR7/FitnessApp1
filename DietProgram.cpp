b95bbf0a2cfb7d99c0743d7e3c3a1c1dbcaa16bd
TIMURRR7 <kalinovskiytimur@bk.ru>
2025-11-30 22:42:34 +0700

    Доработан класс DietProgram

--- DietProgram.cpp
+++ DietProgram.cpp
@@ -1,24 +1,64 @@
-﻿#include "DietProgram.hpp"
+#include "DietProgram.hpp"
+#include <stdexcept>
+
+
+int DietProgram::totalPrograms = 0;
 
 // Конструктор: Инициализирует все поля
 DietProgram::DietProgram(const std::string& id, const std::string& ownerUserId, const std::string& planDate,
     const std::list<std::string>& dailyMeals, const std::string& dailyTargets)
-    : id(id), ownerUserId(ownerUserId), planDate(planDate), dailyMeals(dailyMeals), dailyTargets(dailyTargets) {}
+    : id(id), ownerUserId(ownerUserId), planDate(planDate), dailyMeals(dailyMeals), dailyTargets(dailyTargets) {
+    totalPrograms++;
+}
+
+// Конструктор копирования
+DietProgram::DietProgram(const DietProgram& other)
+    : id(other.id), ownerUserId(other.ownerUserId), planDate(other.planDate),
+    dailyMeals(other.dailyMeals), dailyTargets(other.dailyTargets) {
+    totalPrograms++;
+}
 
 // Деструктор
-DietProgram::~DietProgram() {}
+DietProgram::~DietProgram() {
+    totalPrograms--;
+}
 
 // Метод: Обновляет по весу (заглушка)
 void DietProgram::updateForWeightShift(double weightDelta) {
-    // Изменение калорийности
+    try {
+        // Изменение калорийности
+        if (weightDelta == 0) {
+            throw std::invalid_argument("Изменение не может быть нулевым");
+        }
+    }
+    catch (const std::exception& e) {
+        throw;
+    }
 }
 
 // Метод: Возвращает суммарные нутриенты (заглушка)
 std::string DietProgram::calcDailySummary() const {
-    return "Summary"; // Пример
+    return "Суммарные нутриенты: 2000 ккал"; // Пример
 }
 
 // Метод: Заменяет блюдо (заглушка)
 void DietProgram::swapDish(const std::string& mealTime, const std::string& replacement) {
-    // Замена
+    try {
+        if (mealTime.empty() || replacement.empty()) {
+            throw std::invalid_argument("Время приема пищи и замена не могут быть пустыми");
+        }
+        // Замена
+    }
+    catch (const std::exception& e) {
+        throw;
+    }
+}
+
+
+bool DietProgram::operator==(const DietProgram& other) const {
+    return this->id == other.id && this->ownerUserId == other.ownerUserId;
+}
+DietProgram& DietProgram::operator+=(const std::string& meal) {
+    this->dailyMeals.push_back(meal);
+    return *this;
 }
