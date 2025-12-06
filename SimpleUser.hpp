85e7f0f820d822e55318e867475c7fce5140787c
TIMURRR7 <kalinovskiytimur@bk.ru>
2025-12-06 21:46:31 +0700

    Доработан SimpleUser

--- SimpleUser.hpp
+++ SimpleUser.hpp
@@ -2,22 +2,40 @@
 #define SIMPLEUSER_HPP
 
 #include "ProfileManager.hpp"
+#include "FitnessComponent.hpp"
 
 class SimpleUser : public ProfileManager {
 public:
-    SimpleUser(const std::string& id, const std::string& username, std::string additionalInfo)
-        : ProfileManager(id, username, 0, ProfileManager::Gender::MALE, 0, 0,
-            ProfileManager::Goal::HEALTH, ProfileManager::Level::BEGINNER,
-            {}, {}, false),
-        additionalInfo(additionalInfo) {}
+    SimpleUser(const std::string& id, const std::string& username, const std::string& email, const std::string& birthDate,
+        Gender gender, int heightCm, double currentWeight, Goal goal, Level level, std::string additionalInfo)
+        : ProfileManager(id, username, 25, gender, heightCm, currentWeight, goal, level,
+            { Weekday::MONDAY }, { Equipment::DUMBBELLS }, true),
+        additionalInfo(additionalInfo)
+    {}
 
-    SimpleUser(const SimpleUser& other) : ProfileManager(other), additionalInfo(other.additionalInfo) {}
+    SimpleUser(const SimpleUser& other) = delete;
+
+    ~SimpleUser() override {
+        std::cout << "SimpleUser destructor" << std::endl;
+    }
+
+   
+    double calculateSomething() const override {
+        return calculateBMI() + 10.0;
+    }
+
+    SimpleUser& operator=(const ProfileManager& base) {
+        if (this != &base) {
+            ProfileManager::operator=(base);
+            additionalInfo = "assigned from base";
+        }
+        return *this;
+    }
+
+    std::string getAdditionalInfo() const { return this->additionalInfo; }
 
 private:
     std::string additionalInfo;
-
-public:
-    std::string getAdditionalInfo() const { return this->additionalInfo; }
 };
 
 #endif
